6e8c39c697780b3346f8dd816502c3ee3b78f4cd
Vault <62789852+VaultGitos@users.noreply.github.com>
2021-05-26 13:47:43 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,6 +6,44 @@
 ██║     ██║██║  ██╗██║  ██║    ██║  ██║╚██████╔╝██████╔╝
 ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝    ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ 
 --]]
+
+local decodeisgay = Instance.new("ScreenGui")
+local TextLabel = Instance.new("TextLabel")
+
+decodeisgay.Name = "decode is gay"
+decodeisgay.Parent = game.CoreGui
+decodeisgay.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+TextLabel.Parent = decodeisgay
+TextLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+TextLabel.BackgroundColor3 = Color3.fromRGB(16, 16, 16)
+TextLabel.BorderColor3 = Color3.fromRGB(68, 68, 68)
+TextLabel.Position = UDim2.new(0.5, 0, 0, 0)
+TextLabel.Size = UDim2.new(0, 5000, 0, 0)
+TextLabel.ZIndex = 0
+TextLabel.Font = Enum.Font.Code
+TextLabel.Text = ""
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 14.000
+TextLabel.TextStrokeTransparency = 0.000
+
+TextLabel:TweenSize(UDim2.new(0, 5000, 0, 20), Enum.EasingDirection.Out, Enum.EasingStyle.Bounce, 1, false)
+wait(1.5)
+TextLabel.Text = "checking exploit environment..."
+wait(0.6)
+TextLabel.Text = "loading framework..."
+wait(0.6)
+TextLabel.Text = "loading ui..."
+wait(0.6)
+TextLabel.Text = "loading config..."
+wait(0.6)
+TextLabel.Text = "finished <3 - pika hub"
+wait(1.5)
+TextLabel.Text = ""
+TextLabel:TweenSize(UDim2.new(0, 5000, 0, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Elastic, 1, false)
+wait(2)
+TextLabel:Destroy()
+
 if pikaloaded then return; end
 pcall(function()
   getgenv().pikaloaded = true
